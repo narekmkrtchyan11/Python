ae01bab18d262a4d705f61a3df8cdf3432d4d34a
Narek <narek.mkrtchyan@picsart.com>
2023-04-05 12:27:36 +0400

    correct code

--- Exam1.py
+++ Exam1.py
@@ -3,27 +3,28 @@
 # def returnTriangle(height, symbol = '#'):
 #     array = []
 #     for i in range(1, height + 1):
-#         arr = [symbol]*i
-#         array.append(arr)
+#         array.append([symbol]*i)
 #     return array
 # print(returnTriangle(3))
+# print(returnTriangle(2, '*'))
 
 # 2 Greatest common divisor
 
-# arr = [1, 2, 3, 4, 5, 6]
-# def gcd (num1,num2):
-#     if (num2 == 0):
-#         return num1
-#     else:
-#          return gcd (num2, num1 % num2)
+arr = [2, 4, 6, 18]
+def gcd(num1, num2):
+    if (num2 == 0):
+        return num1
+    else:
+         return gcd(num2, num1 % num2)
 
-# def getMaxDivisor(arr):
-#     max = arr[0]
-#     for i in arr[1:]:
-#         max = gcd(max , i)
-#     return max
+def getMaxDivisor(arr):
+    max = arr[0]
+    for i in arr[1:]:
+        print(1111, i)
+        max = gcd(max , i)
+    return max
 
-# print(getMaxDivisor(arr))
+print(getMaxDivisor(arr))
 
 # 3 Recursive Fibonacci
 
@@ -56,17 +57,15 @@
 
 # 5 Jumping Frog
 
-def jumping_frog(P, h):
-    for i in range(1, len(P)):
-        if P[i] - P[i - 1] == 2 or P[i] - P[i - 1] == -2:
-            continue
-        else:
-            return 'Game over'
-    return 'Frog wins'
+# def jumping_frog(P, h):
+#     for i in range(1, len(P)):
+#         if abs(P[i] - P[i - 1])  <= h:
+#             continue
+#         else:
+#             return 'Game over'
+#     return 'Frog wins'
 
-
-
-print(jumping_frog([4,5, 2], 2))
+# print(jumping_frog([4,5, 2], 3))
 
     
 
